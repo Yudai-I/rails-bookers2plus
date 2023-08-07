8291e7d4a8d58af858b658822fdd9e8d44aca58c
YudaiInoue <139064232+YudaiInoue@users.noreply.github.com>
2023-08-07 01:43:52 +0000

    [Finish] 課題3

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable
+         
+         
+ has_many :books, dependent: :destroy
+ 
+       validates :name, length:{ in: 2..20 }
+       validates :name, uniqueness: true
+       validates :email, uniqueness: true
+          validates :introduction, length:{maximum: 50 }
+         validates :email,presence: true
+         validates :password,presence: true,on: :registration
+      
+         
+         
+         has_one_attached :profile_image
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+    root to: 'homes#top'
+  get '/home/about' => 'homes#about'
+  resources :books, only: [:index, :new, :create, :show, :edit, :update, :destroy, :create]
+  resources :users, only: [:index, :create, :show, :edit, :update, :destroy]
+  
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
